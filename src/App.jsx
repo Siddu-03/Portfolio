9e9bd6de4bf6a0b57d4ab37797d3cd358108b285
Siddeshwarprasad K R <siddulukcy@gmail.com>
2026-08-22 19:00:24 +0530

    new additions

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Home from "../structural/Home.jsx";
+import About from "../structural/About.jsx";
+import Work from "../structural/Work.jsx";
+import Photography from "../structural/Photography.jsx";
+import Contact from "../structural/Contact.jsx";
+import CaseStudy from "../structural/CaseStudy.jsx";
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/work" element={<Work />} />
+        <Route path="/work/:projectId" element={<CaseStudy />} />
+        <Route path="/photography" element={<Photography />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
